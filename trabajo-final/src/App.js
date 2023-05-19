7861ebe207587890dcdb8bb1b821064887e4723f
jdpatino <juan.patco@gmail.com>
2023-05-19 02:13:46 -0500

    first commit

--- trabajo-final/src/App.js
+++ trabajo-final/src/App.js
@@ -8,14 +8,14 @@ import education from "./education";
 import SkillsList from "./SkillsList";
 
 const info = {
-  imgSrc: "./FotoPL.png",
-  name: "Paula Latorre",
-  profession: "Industrial Civil Engineer",
-  address: "Lund",
-  phone: 72091084840,
-  email: "skjhf@gmail.com",
+  imgSrc: "./Foto_cv.png",
+  name: "Juan Diego Patiño",
+  profession: "Ingeniero en Sistemas y Telecomunicaciones",
+  address: "cll 66 9a-119",
+  phone: 3215088281,
+  email: "juan.patco@gmail.com",
   about:
-    "Experience in project coordination, team and customer management. What I've loved most of every job I've had is established new networks based on trust, because it makes everything thrive. My current focus is to become a Web Developer and an E-Commerce expert."
+    "Siempre preparado para nuevos retos, receptivo a nuevo conocimiento, lucha constante para ser uno de los mejores desarrolladores web"
 };
 
 export default function App() {
--- trabajo-final/src/education.js
+++ trabajo-final/src/education.js
@@ -1,19 +1,19 @@
 const education = [
   {
     id: 1,
-    startDate: "Sept 2019",
-    endDate: "May 2020",
-    title: "Web Development / FooCafe",
+    startDate: "January 2011",
+    endDate: "December 2013",
+    title: "Tecnologo en Mantenimiento Mecanico Industrial",
     description:
-      "FooCoding: is a six month coding course in web development taught by professional developers. https://www.foocoding.org/curriculum/"
+      "Tecnologo en Mantenimiento Predictivo,Programado y Correctivo de maquinaria Industrial"
   },
   {
     id: 2,
-    startDate: "March 2000",
-    endDate: "May 2007",
-    title: "Civil Industrial Engineering / University of Chile",
+    startDate: "June 2020",
+    endDate: "December 2025",
+    title: "Ingenieria en Sistemas y Telecomunicaciones",
     description:
-      "Six-year professional study with a focus in Organizational Management – concepts and methodologies from operations management, information and communication technologies, finances, industrial economics, and marketing."
+      "Conocer y asumir posiciones críticas sobre las normas y políticas en el campo de los sistemas y las telecomunicaciones."
   }
 ];
 
