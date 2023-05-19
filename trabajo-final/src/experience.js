3ec227b1c71b4ca9cebf40e63ed49c1218a944a1
jdpatino <juan.patco@gmail.com>
2023-05-19 17:57:50 -0500

    Experiencia laboral

--- trabajo-final/src/experience.js
+++ trabajo-final/src/experience.js
@@ -1,29 +1,29 @@
 const experience = [
   {
     id: 3,
-    startDate: "Sep 2019",
+    startDate: "December 2013",
     endDate: "current",
-    title: "Online-shop Owner / Olivias Garderob",
+    title: "Implementacion Emergia",
     description:
-      "Online vintage and preloved clothing store for chic and sustainable women <https://oliviasgarderob.com/>",
+      "Desarrollo e implementacion de servicios de contact center",
     image: "./ropa-portada.png"
   },
   {
     id: 2,
-    startDate: "Sep 2018",
-    endDate: "Jun 2019",
-    title: "Key Accout Manager Spain (Intern) / Renjer Snacks AB",
+    startDate: "Sep 2012",
+    endDate: "Jun 2013",
+    title: "Mecanico Industrial",
     description:
-      "Responsible for identify and contact new potential clients in Spain (unexplored market for the company)",
+      "Responsable del mantenimiento programado de maquinaria industrial",
     image: "./renjer.png"
   },
   {
     id: 1,
-    startDate: "April 2013",
-    endDate: "March 2017",
-    title: "Entrepreneurship Manager / Women's Business Club S.A",
+    startDate: "Jun 2013",
+    endDate: "Nov 2013",
+    title: "Agente en ventas comeciales",
     description:
-      "In charge of the implementation and operation of the first business accelerator in Latin America for female entrepreneurs .",
+      "Gestor de ventas para empresa de telecomunicaciones de la localidad",
     image:
       "https://image.freepik.com/free-photo/hand-putting-coins-stack-with-plant-growing-money-concept-finance-accounting_34152-1989.jpg"
   }
